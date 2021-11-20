1f68170e2381045c1ccbed68d7694a48c19672c0
YarDol <94075933+YarDol@users.noreply.github.com>
2021-11-20 18:52:13 +0200

    Add files via upload

---
+++ Errors.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+grep -E  "404" log.txt 
+grep  "404" log.txt -c  
---
+++ Yar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Скопійовано файли"
+
+cp ./Dolgushyn/DRR/Yar.txt ./Dolgushyn
+cp ./Dolgushyn/DRR/Dolgushyn.txt ./Dolgushyn
+
+
